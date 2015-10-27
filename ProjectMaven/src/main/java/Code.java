f1d2e020088a8d6145ffe1e76494866429afa5b3
palak3134 <palak3134@gmail.com>
2015-10-27 11:47:15 +0530

    Training 3

---
+++ ProjectMaven/src/main/java/Code.java
@@ -0,0 +1,12 @@
+
+public class Code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+	public int cal() {
+		return 0;
+	}
+
+}
---
+++ ProjectMaven/src/test/java/CodeTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CodeTest {
+
+	@Test
+	public void test() {
+		assertEquals(0, new Code().cal());
+	}
+
+}
